2c6ba22b97c76ac1382d12c2f0f140ad45912c65
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-11 22:34:33 +0500

    написал открытие файла

--- helo_gree/helo.go
+++ helo_gree/helo.go
@@ -8,15 +8,23 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"example.com/greetings"
 )
 
 func main() {
+
+	// Открываю файл, если не получается - выдаю ошибку
+	file, err := os.OpenFile("helo.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	log.SetPrefix("Greetings: ")
 	//log.SetFlags(0)
+	if err != nil {
+		log.Fatalf("Не удалось открыть файл %v", err)
+	}
+	defer file.Close()
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Milya")
 	if err != nil {
 		log.Fatal(err)
 	}
